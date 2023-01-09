23b7f8021919b8dff3a8c0a53252c9b6c81e223e
ddfesfes <janghs3071@gmail.com>
2023-01-09 21:40:14 +0900

    .

---
+++ commands/example.py
@@ -0,0 +1,5 @@
+description = "geq"
+response = "gefa"
+
+async def example(ctx):
+	await ctx.respond(response)
--- commands/hiworld.py
+++
@@ -1,5 +0,0 @@
-description = "yeshi"
-response = "ohHi"
-
-async def hiworld(ctx):
-	await ctx.respond(response)
